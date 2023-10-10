1b653f41233dae9fc6750ef88a1c9312d02b3eaa
adel khedri mirqaid <adel.khedri.mirqaid@gmail>
2023-10-10 14:13:23 +0330

    create pament app models and shop app models

--- shop/payment/urls.py
+++ shop/payment/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('cards/', views.CardManager.as_view(), name="card manager"),
 ]
--- shop/payment/views.py
+++ shop/payment/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+
+class CardManager(View):
+    template_name = "payment/card-manager.html"
+
+    def get(self, request):
+        return render(request, self.template_name, {})
